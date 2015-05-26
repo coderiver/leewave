901bc175ff2555b091c34fb353820ebd1829f930
Eugene Godun <eugene.godun@gmail.com>
2015-05-26 13:48:47 +0300

    index part 1

--- js/common.js
+++ js/common.js
@@ -15,5 +15,37 @@ head.ready(function() {
 	//     scrollFixedElements()
 	// });
 
-	console.log($('body').html());
+	var configCat = {
+		slidesToShow: 4,
+		dots: false,
+		arrows: false
+	}
+	$(".js-slider-cat").slick(configCat);
+
+	var config = {
+		slidesToShow: 1,
+		dots: false,
+		arrows: false
+	}
+	$('.js-slider').on('init', function(slick) {
+		  setTimeout(function(){
+		  	$('.slider').addClass("is-ready");
+		  },200);
+	});
+	$(".js-slider").slick(config);
+
+	
+	$('.js-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$('.js-slider-nav li').removeClass("is-active");
+		$('.js-slider-nav a[data-slide="'+nextSlide+'"]').parent().addClass("is-active");
+	});
+
+	$(".js-slider-nav a").on("click", function(){
+		var index = $(this).attr("data-slide");
+		$(".js-slider-nav li").removeClass("is-active");
+		$(this).parent().addClass("is-active");
+		$('.js-slider').slick('slickGoTo', index);
+		return false;
+	});
+	        
 });
